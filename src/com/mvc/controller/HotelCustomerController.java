c36948950524b2adb065b7aa5b8ac7e239d9c685
huiminjun <2802076526@qq.com>
2017-01-16 17:06:21 +0800

    慧敏君更新：为了和师兄师姐保持一致

--- src/com/mvc/controller/HotelCustomerController.java
+++ src/com/mvc/controller/HotelCustomerController.java
@@ -28,7 +28,7 @@ import net.sf.json.JSONObject;
  */
 @Controller
 @RequestMapping("/customerServiceInformation")
-public class CustomerServiceInformationController {
+public class HotelCustomerController {
 	@Autowired
 	HotelCustomerService hotelCustomerService;
 	

9cc702be0e84b56f1e88121669be8c87d2489d8d
yiyinana <zhang906337636@163.com>
2017-02-20 19:59:12 +0800

    张姣娜：工程部员工工作量统计后台除图片以外完成

--- src/com/base/constants/ReportFormConstants.java
+++ src/com/base/constants/ReportFormConstants.java
@@ -20,6 +20,7 @@ public class ReportFormConstants {
 	// word模版所在包名
 	public static final String DICTIONARY = "word\\";
 
+	/********************* 客房部相关 **************************************/
 	// 客房部员工工作量汇总表
 	public static final String WORKLOAD_PATH = DICTIONARY + "workLoadSummary.docx";
 	// 客房部员工工作量饱和度分析表
@@ -76,5 +77,12 @@ public class ReportFormConstants {
 	public static final String ROOMWORKLOAD_PATH = DICTIONARY + "roomWorkload.docx";
 	// 导出部门对客服务类型统计
 	public static final String ROOMWORKTYPE_PATH = DICTIONARY + "roomType.docx";
+
+	/********************* 工程部相关 **************************************/
+	// 导出部门对客服务类型统计
+	public static final String PROWORKLOAD_PATH = DICTIONARY + "proWorkLoad.docx";
+
+	// 客房部单个员工工作量分析图
+	public static final String PROWORKLOADANALYSE_PATH = DICTIONARY + "proWorkLoadAnalyse.docx";
 
 }

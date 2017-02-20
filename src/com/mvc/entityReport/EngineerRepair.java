453783792c30a2931e1e74e4a4151c409cf128f4
huiminjun <2802076526@qq.com>
2017-02-20 11:41:24 +0800

    慧敏君更新：工程

---
+++ src/com/mvc/entityReport/EngineerRepair.java
@@ -0,0 +1,50 @@
+package com.mvc.entityReport;
+/**
+ * 工程维修项统计分析
+ * @author wanghuimin
+ * @date 2017年2月20日
+ */
+public class EngineerRepair {
+	private String orderNum;// 序号
+	private String repairParentType;//维修父类型
+	private String repairType;//维修子类型
+	private String serviceLoad;//服务数量
+	private String engineerAmount;//总计
+	
+	
+	public String getOrderNum() {
+		return orderNum;
+	}
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+	public String getRepairParentType() {
+		return repairParentType;
+	}
+	public void setRepairParentType(String repairParentType) {
+		this.repairParentType = repairParentType;
+	}
+	public String getRepairType() {
+		return repairType;
+	}
+	public void setRepairType(String repairType) {
+		this.repairType = repairType;
+	}
+	public String getServiceLoad() {
+		return serviceLoad;
+	}
+	public void setServiceLoad(String serviceLoad) {
+		this.serviceLoad = serviceLoad;
+	}
+	public String getEngineerAmount() {
+		return engineerAmount;
+	}
+	public void setEngineerAmount(String engineerAmount) {
+		this.engineerAmount = engineerAmount;
+	}
+	
+	
+	
+	
+
+}

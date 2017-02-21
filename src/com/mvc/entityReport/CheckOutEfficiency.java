264e82365379e5c2a9847ebf52c3721e283aeedd
aaron1430 <aaron1430@163.com>
2017-02-21 16:10:44 +0800

    包阿儒汉：查退房后台基本完成

---
+++ src/com/mvc/entityReport/CheckOutEfficiency.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.mvc.entityReport;
+
+/**
+ * @author 包阿儒汉
+ *
+ */
+public class CheckOutEfficiency {
+	private String orderNum;// 序号
+	private String authorName;// 员工姓名
+	private String authorNo;//员工编号
+	private String sumTime;// 总用时（分钟）
+	private String givenTime;// 平均给定时间（分钟）
+	private String usedTimeAvg;// 平均抢房用时（分钟）
+	private String workCount;// 抢房总数
+	private String workEffeciencyAvg;// 平均抢房效率
+	private String timeOutRate;// 超时率
+
+	public String getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getSumTime() {
+		return sumTime;
+	}
+
+	public void setSumTime(String sumTime) {
+		this.sumTime = sumTime;
+	}
+
+	public String getGivenTime() {
+		return givenTime;
+	}
+
+	public void setGivenTime(String givenTime) {
+		this.givenTime = givenTime;
+	}
+
+	public String getUsedTimeAvg() {
+		return usedTimeAvg;
+	}
+
+	public void setUsedTimeAvg(String usedTimeAvg) {
+		this.usedTimeAvg = usedTimeAvg;
+	}
+
+	public String getWorkCount() {
+		return workCount;
+	}
+
+	public void setWorkCount(String workCount) {
+		this.workCount = workCount;
+	}
+
+	public String getWorkEffeciencyAvg() {
+		return workEffeciencyAvg;
+	}
+
+	public void setWorkEffeciencyAvg(String workEffeciencyAvg) {
+		this.workEffeciencyAvg = workEffeciencyAvg;
+	}
+
+	public String getTimeOutRate() {
+		return timeOutRate;
+	}
+
+	public void setTimeOutRate(String timeOutRate) {
+		this.timeOutRate = timeOutRate;
+	}
+
+	public String getAuthorNo() {
+		return authorNo;
+	}
+
+	public void setAuthorNo(String authorNo) {
+		this.authorNo = authorNo;
+	}
+
+}

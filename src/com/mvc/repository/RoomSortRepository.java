6761f64e56ecbda8a401bed8dd264796161ab589
haishangqishi <ahaishangqishi@163.com>
2017-02-23 16:09:46 +0800

    客房部做房用时，效率加入报表统计

--- src/com/mvc/repository/RoomSortRepository.java
+++ src/com/mvc/repository/RoomSortRepository.java
@@ -1,6 +1,8 @@
 package com.mvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mvc.entity.RoomSort;
 
@@ -12,4 +14,7 @@ import com.mvc.entity.RoomSort;
  */
 public interface RoomSortRepository extends JpaRepository<RoomSort, Integer> {
 
+	// 根据sort_no获取房间类型
+	@Query("select rs from RoomSort rs where sort_no=:sort_no")
+	RoomSort selectNameBySortNo(@Param("sort_no") String sort_no);
 }
